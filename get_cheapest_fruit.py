cc5e54a478d2b16c8c020073987ac815cf05e37c
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-14 15:05:30 +0500

    cheapest_fruit

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -1,4 +1,5 @@
-def get_cheapest_fruit(data:str)->str:
+import csv
+def get_cheapest_fruit(fname:str)->str:
     """
     This function returns the name of the cheapest fruit in the list
 
@@ -7,5 +8,19 @@ def get_cheapest_fruit(data:str)->str:
     returns:
         name of the cheapest fruit
     """
-    # your code here
-    pass
+    file = open('fruits.csv')
+    reader = csv.reader(file)
+    lst1 = []
+    lst2 = []
+    for row in reader:
+        if row[1] != 'price':
+            lst1.append(row[0])
+            lst2.append(row[1])
+    s = ''
+    min = lst2[0]
+    for i in range(1,len(lst2)):
+        if float(lst2[i]) < float(min):
+            min = lst2[i]
+            s = lst1[i]
+    return s
+print(get_cheapest_fruit('fruits.csv'))
