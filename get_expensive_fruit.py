f8dc93fe29b7b917ab9b9b8a1d8f9bac9d3d3874
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-14 14:53:58 +0500

    expensive_fruit

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -1,4 +1,5 @@
-def get_expensive_fruit(data:str)->str:
+import csv
+def get_expensive_fruit(fname:str)->str:
     """
     This function returns the name of the most expensive fruit in the list
 
@@ -7,7 +8,22 @@ def get_expensive_fruit(data:str)->str:
     returns:
         name of the most expensive fruit
     """
-    # your code here
-    return  
+    file = open('fruits.csv')
+    reader = csv.reader(file)
+    lst1 = []
+    lst2 = []
+    for row in reader:
+        if row[1] != 'price':
+            lst1.append(row[0])
+            lst2.append(row[1])
+    s = ''
+    max = lst2[0]
+    for i in range(1,len(lst2)):
+        if float(lst2[i]) > float(max):
+            max = lst2[i]
+            s = lst1[i]
+    return s
+print(get_expensive_fruit('fruits.csv'))
+     
 
 
